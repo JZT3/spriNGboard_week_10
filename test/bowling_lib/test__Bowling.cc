f5f34001c2b001bff06a6a64182284ccf0764f3c
jzt3 <jzt3@example.com>
2023-04-01 16:13:13 -0700

    First passing test

--- test/bowling_lib/test__Bowling.cc
+++ test/bowling_lib/test__Bowling.cc
@@ -1,9 +1,32 @@
+  
 #include "bowling.h"
-
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE ("this test will fail")
+class BowlGame
 {
-  REQUIRE(false);
+  public:
+    bool is_started() const;
+};
+
+bool BowlGame::is_started() const
+{
+  return true;
 }
 
+TEST_CASE ("All pins are standing and no ball has been thrown")
+{
+  // Arrange
+  BowlGame bg;
+
+  // Act
+  // do nothing
+
+  // Assert
+
+  REQUIRE(bg.is_started() == true);
+}
+
+TEST_CASE("")
+{
+  
+}
